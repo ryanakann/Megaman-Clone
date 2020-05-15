00aec190078cfd74b2dfdfe19972903241cb593b
ryanakann <ryanakann@gmail.com>
2020-05-14 21:40:22 -0400

    Update CustomRigidbody2D.cs

--- Megaman Project/Assets/Scripts/CustomRigidbody2D.cs
+++ Megaman Project/Assets/Scripts/CustomRigidbody2D.cs
@@ -23,21 +23,23 @@ public class CustomRigidbody2D : MonoBehaviour {
         Vector2 mySize = me.bounds.size;
         Vector2 otherCenter = (Vector2)other.transform.position + other.bounds.center;
         Vector2 otherSize = other.bounds.size;
-        float xDist = otherCenter.x - myCenter.x;
-        float yDist = otherCenter.y - myCenter.y;
+        float xSignedDist = otherCenter.x - myCenter.x;
+        float xDist = Mathf.Abs(xSignedDist);
+        float ySignedDist = otherCenter.y - myCenter.y;
+        float yDist = Mathf.Abs(ySignedDist);
         float xTargetDist = (mySize.x + otherSize.x) / 2f;
         float yTargetDist = (mySize.y + otherSize.y) / 2f;
-        if (Mathf.Abs(xDist) < xTargetDist && Mathf.Abs(xDist) > Mathf.Abs(yDist)) {
-            transform.position -= Vector3.right * (xTargetDist - Mathf.Abs(xDist)) * Mathf.Sign(xDist);
-            velocity.x = 0;
+        if (xDist < xTargetDist && velocity.x * xSignedDist > 0f && xDist > yDist) {
+            transform.position -= Vector3.right * (xTargetDist-xDist) * Mathf.Sign(xSignedDist);
+            velocity.x = 0f;
         }
-        if (Mathf.Abs(yDist) < yTargetDist && Mathf.Abs(yDist) > Mathf.Abs(xDist)) {
-            transform.position -= Vector3.up * (yTargetDist - Mathf.Abs(yDist)) * Mathf.Sign(yDist);
-            velocity.y = 0;
+        if (yDist < yTargetDist && velocity.y * ySignedDist > 0f && yDist > xDist) {
+            transform.position -= Vector3.up * (yTargetDist-yDist) * Mathf.Sign(ySignedDist);
+            velocity.y = 0f;
         }
 
         //Grounded threshold
-        if (yDist < yTargetDist + 0.01f) {
+        if (ySignedDist < yTargetDist + 0.01f) {
             grounded = true;
         }
     } 
