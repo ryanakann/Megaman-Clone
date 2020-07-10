50439ce20463803696dbea0cefd2515c14a176e5
ryanakann <ryanakann@gmail.com>
2020-07-10 10:36:16 -0400

    Tweens

---
+++ Megaman Project/Assets/MenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+
+// public delegate void 
+
+public class MenuManager : MonoBehaviour {
+
+    public UnityEvent OnHealthChange;
+
+    public void StartGame () {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Megaman Project/Assets/Tween.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tween : MonoBehaviour
+{
+    public AnimationCurve tween;
+
+    [Tooltip("In seconds")]
+    public float tweenDuration = 1f;
+    private Vector3 startPos;
+    private Vector3 endPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        endPos = transform.position + Vector3.down * 4f;
+
+        InvokeRepeating("PlayTween", 1f, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlayTween () {
+        StartCoroutine(PlayTweenCR());
+    }
+
+    IEnumerator PlayTweenCR () {
+        float t = 0f;
+        float tTween;
+        while (t <= 1f) {
+            tTween = tween.Evaluate(t);
+            transform.position = Vector3.Lerp(startPos, endPos, tTween);
+            t += Time.deltaTime / tweenDuration;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
